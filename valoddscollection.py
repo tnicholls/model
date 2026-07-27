4b1d787692e1c62c9d071311283d6d78b5811981
Tom Nicholls <tnicholls@uchicago.edu>
2026-07-27 13:01:18 -0500

    what you changed

--- valoddscollection.py
+++ valoddscollection.py
@@ -1,1 +1,6 @@
-dfdfd
+
+# scrape betting odds closing lines for all valorant matches, 
+# compute the implied odds, the vig (you will probably need to calculate this
+# from running tests on whatever betting platform used, e.g. on pinnacle get the
+# average of the last 10 closing vigs as an estimate) and collect the data 
+# export it to an excel/sheet so its useable
